1b29440a972160c6a867471348852f99369a189e
GodHongIl <adunstudio@daum.net>
2020-08-21 02:01:32 +0900

    BGDatabase 에셋 / 데이터 로직

---
+++ Assets/Script/BalanceData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BalanceData
+{
+    public static int newQuarantine   => TBL_DATA.GetEntity(0).newQuarantine;
+    public static float minFoodMulti => TBL_DATA.GetEntity(0).minFoodMulti;
+}
