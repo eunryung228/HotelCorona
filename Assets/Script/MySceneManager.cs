54bac6f64b4523af25a80ef02017e4cef5657881
eunryung228 <ryaner@khu.ac.kr>
2020-08-21 01:04:17 +0900

    add scenemanager

---
+++ Assets/Script/MySceneManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+    public Image fadeImage;
+
+    private string currScene = "TitleScene";
+    private Color m_color;
+
+    private float fadeTime = 1f;
+    float imgStart;
+    float imgEnd;
+    float imgTime = 0f;
+
+
+    public void ChangeScene(string scene)
+    {
+        StartCoroutine(ChangeSceneCoroutine(scene: scene));
+    }
+
+    IEnumerator ChangeSceneCoroutine(string scene)
+    {
+        yield return StartCoroutine(ImageFadeOut());
+        SceneManager.UnloadSceneAsync(currScene);
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        StartCoroutine(ImageFadeIn());
+    }
+
+
+    protected IEnumerator ImageFadeIn()
+    {
+        m_color.a = 1f;
+        m_color = fadeImage.color;
+        imgStart = 1f; imgEnd = 0f; imgTime = 0f;
+
+        while (m_color.a > 0f)
+        {
+            imgTime += Time.deltaTime / fadeTime;
+            m_color.a = Mathf.Lerp(imgStart, imgEnd, imgTime);
+            fadeImage.color = m_color;
+            yield return null;
+        }
+        fadeImage.gameObject.SetActive(false);
+    }
+
+
+    protected IEnumerator ImageFadeOut()
+    {
+        fadeImage.gameObject.SetActive(true);
+        m_color.a = 0f;
+        m_color = fadeImage.color;
+        imgStart = 0f; imgEnd = 1f; imgTime = 0f;
+        m_color.a = Mathf.Lerp(imgStart, imgEnd, imgTime);
+
+        while (m_color.a < 1f)
+        {
+            imgTime += Time.deltaTime / fadeTime;
+            m_color.a = Mathf.Lerp(imgStart, imgEnd, imgTime);
+            fadeImage.color = m_color;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Script/TitleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("UIScene", LoadSceneMode.Additive);
+    }
+
+    public void ClickGameStart()
+    {
+        FindObjectOfType<MySceneManager>().ChangeScene("TempMainScene");
+    }
+
+    public void ClickGameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+}
