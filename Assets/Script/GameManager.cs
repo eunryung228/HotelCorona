9f7f6ecc96b20a1aaf783ab8fa23c7c446c977c0
GodHongIl <adunstudio@daum.net>
2020-08-23 11:44:24 +0900

    격리해제 0명 수정

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -294,7 +294,7 @@ public partial class GameManager : MonoBehaviour, GameEventListener<GameEvent>
         {
             AudioManager.Instance.Play("Ambulance");
             GameEvent.Trigger(GameEventType.PageChange);
-            PopupEvent.Trigger(PopupEventType.Confirm, 0);
+            PopupEvent.Trigger(PopupEventType.Confirm, count);
         }
         
         confirmNum += count;
@@ -320,7 +320,7 @@ public partial class GameManager : MonoBehaviour, GameEventListener<GameEvent>
 
         if (count > 0)
         {
-            PopupEvent.Trigger(PopupEventType.Cure, 0);
+            PopupEvent.Trigger(PopupEventType.Cure, count);
         }
         
         cureNum += count;
